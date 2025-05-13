4852038411bbe2970b2c4f209999e52eb8c469fc
bmxciel <brendaj2004@gmail.com>
2025-05-13 16:04:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function mensagemespecial() {
+    document.getElementById("mensagem").textContent =
+        "Você é incrível! Seja você mesmo e procure sempre dar o seu melhor em tudo, vai da tudo certo! 💪✨";
+}
